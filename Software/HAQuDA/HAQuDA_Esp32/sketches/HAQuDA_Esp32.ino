7a9e750b94d044d8ec2bc5cb6a56703b7eb279e6
Vitaliy Orazov <viordash@mail.ru>
2021-06-05 20:11:12 +0300

    initial configss for use Blynk lib for remote control

--- Software/HAQuDA/HAQuDA_Esp32/sketches/HAQuDA_Esp32.ino
+++ Software/HAQuDA/HAQuDA_Esp32/sketches/HAQuDA_Esp32.ino
@@ -1,14 +1,53 @@
-#define LED_BUILTIN 2
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#include "Board.h"
+
+char auth[] = "Oi4mueBWA22xekeJ3Ppj7m95-wh0spEH";
+char ssid[] = "ilor";
+char pass[] = "ilor66142222!";
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info);
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info);
+void UpdateVirtualPins();
 
 void setup() {
 	Serial.begin(115200);
 	pinMode(LED_BUILTIN, OUTPUT);
+
+	WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+	WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, pass, 0, 0);
+
+	Blynk.config(auth, BLYNK_DEFAULT_DOMAIN, BLYNK_DEFAULT_PORT);
 }
 
 void loop() {
-	log_i("Blinking...");
+	wl_status_t status = WiFi.status();
+	if (status != WL_CONNECTED) {
+		log_i("Connecting to %s", ssid);
+		WiFi.reconnect();
+		uint32_t timer = millis();
+
+		while (WiFi.status() != WL_CONNECTED && !PeriodInRange(timer, 5000)) {
+			delay(200);
+		}
+	} else if (!Blynk.connected()) {
+		Blynk.connect();
+	} else {
+		Blynk.run();
+	}
+}
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
 	digitalWrite(LED_BUILTIN, HIGH);
-	delay(500);
+	log_i("WiFiStationConnected");
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
 	digitalWrite(LED_BUILTIN, LOW);
-	delay(500);
+	log_i("WiFiStationDisconnected");
 }
