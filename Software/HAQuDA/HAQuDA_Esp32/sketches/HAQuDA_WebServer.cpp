e2ae2c74ab9e5b825000f1eaedad79b4656b75b5
Ilor5 <ilordash@mail.ru>
2022-03-26 20:14:30 +0300

    updated func for getting WiFi creds

--- Software/HAQuDA/HAQuDA_Esp32/sketches/HAQuDA_WebServer.cpp
+++ Software/HAQuDA/HAQuDA_Esp32/sketches/HAQuDA_WebServer.cpp
@@ -4,30 +4,49 @@
 HAQuDA_WebServer::HAQuDA_WebServer() {
 }
 
-void HAQuDA_WebServer::begin() {
+void HAQuDA_WebServer::beginWebServer() {
 	server.begin(80);
+	myFS_WiFi->Start();
 }
 
-void HAQuDA_WebServer::setWiFiCreds() {
-	if (server.hasArg("ssid") && server.hasArg("password")) {
-		String ssidNew = server.arg("ssid");
-		String passNew = server.arg("password");
-		log_i("Saving WiFi net with SSID = %s\r\n", ssidNew.c_str());
-		WiFiCredsFound = true;
+void HAQuDA_WebServer::loop() {
+	server.handleClient();
+}
 
-		server.send(200, "text/html", WiFiCredsPage);
-		// server.stop();
+void HAQuDA_WebServer::getNewWiFiCreds() {
+	TWiFiCreds *newWiFiCreds;
+	if (server.hasArg("ssid") && server.hasArg("password")) {
+
+		newWiFiCreds->ssid = server.arg("ssid");
+		newWiFiCreds->password = server.arg("password");
+		// log_i("Saving WiFi net with SSID = %s\r\n", ssidNew.c_str());
+
+		if (myFS_WiFi->AppendFile(FILE_NAME_WIFI_NET, (uint8_t *)newWiFiCreds, sizeof(newWiFiCreds))) {
+			String response_success = "<h1>Success</h1>";
+			response_success += "<h2>Device will restart in 3 seconds</h2>";
+			server.send(200, "text/html", response_success);
+			delay(3000);
+			ESP.restart();
+		} else {
+			String response_error = "<h1>Error writing new WiFi creds in file system</h1>";
+			response_error += "<h2><a href='/'>Go back</a>to try again";
+			server.send(200, "text/html", response_error);
+		}
+
 	} else {
 		// If one of the creds is missing, go back to form page
-		server.send(200, "text/html", WiFiCredsPage);
+		String response_error = "<h1>Error check WiFi creds</h1>";
+		response_error += "<h2><a href='/'>Go back</a>to try again";
+		server.send(200, "text/html", response_error);
 	}
 }
 
 void HAQuDA_WebServer::handle_NotFound() {
-	server.send(404, "text/plain", "Not found");
+	server.send(404, "text/plain", "Not found test");
 }
 
 void HAQuDA_WebServer::WebServer_init() {
-	server.on("/", [this] { setWiFiCreds(); });
+	server.on("/", HTTPMethod::HTTP_GET, [this] { server.send(200, "text/html", WiFiCredsPage); });
+	server.on("/", HTTPMethod::HTTP_POST, [this] { getNewWiFiCreds(); });
 	server.onNotFound([this] { handle_NotFound(); });
 }
