3bf54636054e83163d6556a0b2c473cc2b38eb60
IlorDash <ilordash@mail.ru>
2022-04-24 23:51:18 +0300

    initial commit for Software Ver 2

--- Software/HAQuDA/HAQuDA_Esp32/sketches/TimeHelper.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIME_HELPER_H
-#define __TIME_HELPER_H
-
-uint32_t GetDuration(uint32_t watchedTime);
-bool PeriodInRange(uint32_t watchedTime, uint32_t minTime, uint32_t maxTime);
-bool PeriodInRange(uint32_t watchedTime, uint32_t minTime);
-
-#endif
