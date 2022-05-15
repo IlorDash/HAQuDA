4830ccc2471e547f136826babe55c4c35299cc88
IlorDash <ilordash@mail.ru>
2022-05-15 14:01:10 +0300

    included Arduino.h

--- Software/HAQuDA/HAQuDA_Esp32/sketches/TimeHelper.h
+++ Software/HAQuDA/HAQuDA_Esp32/sketches/TimeHelper.h
@@ -1,6 +1,8 @@
 #ifndef __TIME_HELPER_H
 #define __TIME_HELPER_H
 
+#include "Arduino.h"
+
 uint32_t GetDuration(uint32_t watchedTime);
 bool PeriodInRange(uint32_t watchedTime, uint32_t minTime, uint32_t maxTime);
 bool PeriodInRange(uint32_t watchedTime, uint32_t minTime);
