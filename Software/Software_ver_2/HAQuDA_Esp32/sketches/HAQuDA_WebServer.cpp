1db76eb4a2b64cacc90c0ce520c4773f928a7b0b
IlorDash <ilordash@mail.ru>
2022-06-15 02:26:48 +0300

    started using ESPAsyncWebServer instead of standard WebServer, added functions for taking parameters of new WiFi creds accroding to ESPAsyncWebServer library Choose ESPAsyncWebServer because it can manipulate with files stored in SPIFFS (send web pages stored in interanl memory instead of RAM) and other features that, i think, will helps in future

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_WebServer.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_WebServer.cpp
@@ -1,52 +1,96 @@
 #include "HAQuDA_WebServer.h"
 #include "WebPages.h"
 
+const char *set_WiFi_creds_params_order[WIFI_CREDS_PARAMS_NUM] = {"ssid", "pass", "ip", "gateway"};
+
 HAQuDA_WebServer::HAQuDA_WebServer() {
 }
 
 void HAQuDA_WebServer::beginWebServer() {
-	server.begin(80);
-	myFS_WiFi->Start();
+	// myFS_WiFi->Start();
+	WebServer_init();
+	server.begin();
 }
 
-void HAQuDA_WebServer::loop() {
-	server.handleClient();
+bool HAQuDA_WebServer::foundNameInParamsArrOrder(String name) {
+	for (int i = 0; i < WIFI_CREDS_PARAMS_NUM; i++) {
+		if (name == set_WiFi_creds_params_order[i]) {
+			return true;
+		}
+	}
+	return false;
 }
 
-void HAQuDA_WebServer::getNewWiFiCreds() {
+bool HAQuDA_WebServer::checkCorrectParams(AsyncWebServerRequest *request) {
+	int params_len = request->params();
+	if (params_len != WIFI_CREDS_PARAMS_NUM) {
+		return false;
+	}
+	for (int i = 0; i < params_len; i++) {
+		AsyncWebParameter *p = request->getParam(i);
+		if (!(p->isPost())) {
+			return false;
+		}
+		if (!foundNameInParamsArrOrder(p->name())) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int HAQuDA_WebServer::getParamVal(AsyncWebServerRequest *request, String param) {
+	for (int i = 0; i < WIFI_CREDS_PARAMS_NUM; i++) {
+		AsyncWebParameter *p = request->getParam(i);
+		if (p->name() == param) {
+			return p->value().toInt();
+		}
+	}
+	return -1;
+}
+
+void HAQuDA_WebServer::handle_NewWiFiCreds(AsyncWebServerRequest *request) {
 	TWiFiCreds *newWiFiCreds;
-	if (server.hasArg("ssid") && server.hasArg("password")) {
 
-		newWiFiCreds->ssid = server.arg("ssid");
-		newWiFiCreds->password = server.arg("password");
+	if (checkCorrectParams(request)) {
+
+		newWiFiCreds->ssid = getParamVal(request, set_WiFi_creds_params_order[0]);
+		newWiFiCreds->password = getParamVal(request, set_WiFi_creds_params_order[1]);
 		// log_i("Saving WiFi net with SSID = %s\r\n", ssidNew.c_str());
 
 		if (myFS_WiFi->AppendFile(FILE_NAME_WIFI_NET, (uint8_t *)newWiFiCreds, sizeof(newWiFiCreds))) {
 			String response_success = "<h1>Success</h1>";
 			response_success += "<h2>Device will restart in 3 seconds</h2>";
-			server.send(200, "text/html", response_success);
+			request->send(200, "text/html", response_success);
 			delay(3000);
 			ESP.restart();
 		} else {
 			String response_error = "<h1>Error writing new WiFi creds in file system</h1>";
 			response_error += "<h2><a href='/'>Go back</a>to try again";
-			server.send(200, "text/html", response_error);
+			request->send(200, "text/html", response_error);
 		}
 
 	} else {
 		// If one of the creds is missing, go back to form page
 		String response_error = "<h1>Error check WiFi creds</h1>";
 		response_error += "<h2><a href='/'>Go back</a>to try again";
-		server.send(200, "text/html", response_error);
+		request->send(200, "text/html", response_error);
 	}
 }
 
-void HAQuDA_WebServer::handle_NotFound() {
-	server.send(404, "text/plain", "Not found test");
+void HAQuDA_WebServer::handle_NotFound(AsyncWebServerRequest *request) {
+	request->send(404);
 }
 
 void HAQuDA_WebServer::WebServer_init() {
-	server.on("/", HTTPMethod::HTTP_GET, [this] { server.send(200, "text/html", WiFiCredsPage); });
-	server.on("/", HTTPMethod::HTTP_POST, [this] { getNewWiFiCreds(); });
-	server.onNotFound([this] { handle_NotFound(); });
+	// Route for root / web page
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(SPIFFS, "/index.html", "text/html"); });
+	server.serveStatic("/", SPIFFS, "/");
+
+	server.on("/add_wifi_creds", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(SPIFFS, "/wifi_manager.html", "text/html"); });
+
+	server.on("/wifi_manager", HTTP_POST, std::bind(&HAQuDA_WebServer::handle_NewWiFiCreds, this, std::placeholders::_1));
+	server.onNotFound(std::bind(&HAQuDA_WebServer::handle_NotFound, this, std::placeholders::_1));
+}
+
+HAQuDA_WebServer::~HAQuDA_WebServer() {
 }
--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_WebServer.h
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_WebServer.h
@@ -1,25 +1,30 @@
 #pragma once
 
 #include <WiFi.h>
-#include <WebServer.h>
-#include <WiFiClient.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
 #include "HAQuDA_FileStorage.h"
+
+#define WIFI_CREDS_PARAMS_NUM 4
 
 class HAQuDA_WebServer {
   private:
-  public:
+  protected:
 	HAQuDA_FileStorage *myFS_WiFi;
 	bool WiFiCredsFound = false;
 
-	WebServer server;
-	WiFiClient client;
+	AsyncWebServer server = AsyncWebServer(80);
 
 	void beginWebServer();
-	void loop();
 
 	HAQuDA_WebServer();
+	~HAQuDA_WebServer();
+
+	bool foundNameInParamsArrOrder(String name);
+	bool checkCorrectParams(AsyncWebServerRequest *request);
+	int getParamVal(AsyncWebServerRequest *request, String param);
 
 	void WebServer_init();
-	void getNewWiFiCreds();
-	void handle_NotFound();
+	void handle_NewWiFiCreds(AsyncWebServerRequest *request);
+	void handle_NotFound(AsyncWebServerRequest *request);
 };
