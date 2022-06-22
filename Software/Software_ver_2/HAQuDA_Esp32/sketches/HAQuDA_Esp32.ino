c32265c7b2cc78b2b94502bf43f0c2402927e722
IlorDash <ilordash@mail.ru>
2022-06-22 15:45:17 +0300

    object ptrs replaced by it's instances

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Esp32.ino
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Esp32.ino
@@ -8,9 +8,7 @@
 
 #include "Sensors.h"
 #include "Tasks.h"
-
-#include <EEPROM.h>
-
+#include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
 
 #define DISP_MEAS_PERIOD 300000 //=5 min in ms
@@ -18,11 +16,11 @@
 
 char BlynkAuth[] = BLYNK_AUTH;
 
-WidgetTerminal *terminal;
-HAQuDA_UI *myUI;
-HAQuDA_DispManip *myDispManip;
-HAQuDA_FileStorage *myFS;
-HAQuDA_WiFi_handler *myWiFi_handler;
+WidgetTerminal terminal(V0);
+HAQuDA_DispManip myDispManip;
+HAQuDA_FileStorage myFS;
+HAQuDA_UI myUI(myDispManip);
+HAQuDA_WiFi_handler myWiFi_handler(myUI, myFS);
 
 void UpdateVirtualPins();
 void blynkPrintLog();
@@ -33,42 +31,28 @@ int thirdDot;
 
 bool dispFirstParam = false;
 
-void setup() {
-	myUI = new HAQuDA_UI(myDispManip);
-	myFS = new HAQuDA_FileStorage();
-	myWiFi_handler = new HAQuDA_WiFi_handler(myUI, myFS);
-	myWiFi_handler->createAP();
-	//myWiFi_handler->begin();
 
-	terminal = new WidgetTerminal(V0);
-	myDispManip = new HAQuDA_DispManip();
+void setup() {
+	myWiFi_handler.WiFi_connect();
+	 if (!myFS.Start()) {
+		log_e("SPIFFS not mounted!");
+		return;
+	}
+	 log_i("SPIFFS mounted successfully");
 
 	Serial.begin(115200);
 
-//	myWiFi_handler->WiFi_handleConnection();
-
-	WS2812_begin();
+	/*WS2812_begin();
 	if (!sensorsBegin()) {
 		WS2812_fillColor(COLOR_RED);
-		terminal->println("FATAL ERROR: Failed to begin sensors");
+		terminal.println("FATAL ERROR: Failed to begin sensors");
 		while (1) {
 		}
 	}
 	createTasks(myUI);
 
-	terminal->println("*************************");
-	terminal->print("START LOGGING");
-
-	// pinMode(LED_BUILTIN, OUTPUT);
-
-	// createAP();
-
-	// Start web server-
-
-	// Serial.print("AP IP address: ");
-	// Serial.println(ip_address);
-
-	// Serial.println("HTTP server started");
+	terminal.println("*************************");
+	terminal.print("START LOGGING");*/
 }
 
 uint16_t measNum = 0;
@@ -77,9 +61,12 @@ uint32_t sensors_meas_time = 0;
 
 void loop() {
 
-	myWiFi_handler->WiFi_handleConnection();
+	myWiFi_handler.WiFi_handleConnection();
 
-	if (!Blynk.connected()) {
+	int i = 0;
+	i++;
+	/*
+	/*if (!Blynk.connected()) {
 		Blynk.connect();
 	} else {
 		Blynk.run();
@@ -115,26 +102,26 @@ void loop() {
 
 		PM_2_5_meas.value = 0;
 		PM_2_5_meas.measNum = 0;
-	}
+	}*/
 }
 
 void checkIfMeasCorrect() {
 	if (!eCO2_meas.newMeasDone) {
-		terminal->println("ERROR: Failed to get eCO2 measurment");
+		terminal.println("ERROR: Failed to get eCO2 measurment");
 	}
 	if (!TVOC_meas.newMeasDone) {
-		terminal->println("ERROR: Failed to get TVOC measurment");
+		terminal.println("ERROR: Failed to get TVOC measurment");
 	}
 	if (!PM_2_5_meas.newMeasDone) {
-		terminal->println("ERROR: Failed to get PM 2.5 measurment");
+		terminal.println("ERROR: Failed to get PM 2.5 measurment");
 	}
 	if (!temp_meas.newMeasDone) {
-		terminal->println("ERROR: Failed to get TEMP measurment");
+		terminal.println("ERROR: Failed to get TEMP measurment");
 	}
 	if (!humid_meas.newMeasDone) {
-		terminal->println("ERROR: Failed to get HUMID measurment");
+		terminal.println("ERROR: Failed to get HUMID measurment");
 	}
-	terminal->flush();
+	terminal.flush();
 
 	eCO2_meas.newMeasDone = false;
 	TVOC_meas.newMeasDone = false;
@@ -144,141 +131,141 @@ void checkIfMeasCorrect() {
 }
 
 void blynkPrintLog() {
-	terminal->println();
-	terminal->println("----------------------------");
-	terminal->print(measNum);
-	terminal->println(" - measurment");
+	terminal.println();
+	terminal.println("----------------------------");
+	terminal.print(measNum);
+	terminal.println(" - measurment");
 
 	checkIfMeasCorrect();
 
-	terminal->print("Temp: ");
-	terminal->print(temp_meas.value / temp_meas.measNum);
-	terminal->println("  C");
-	terminal->print("Humid: ");
-	terminal->print(humid_meas.value / humid_meas.measNum);
-	terminal->println("  %");
-	terminal->println();
+	terminal.print("Temp: ");
+	terminal.print(temp_meas.value / temp_meas.measNum);
+	terminal.println("  C");
+	terminal.print("Humid: ");
+	terminal.print(humid_meas.value / humid_meas.measNum);
+	terminal.println("  %");
+	terminal.println();
 
-	terminal->flush();
+	terminal.flush();
 
-	terminal->print("PM1.0: ");
-	terminal->print(PM_1_0_meas.value / PM_1_0_meas.measNum);
-	terminal->println("  ug/m3");
-	terminal->print("PM2.5: ");
-	terminal->print(PM_2_5_meas.value / PM_2_5_meas.measNum);
-	terminal->println("  ug/m3");
-	terminal->print("PM1 0: ");
-	terminal->print(PM_10_meas.value / PM_10_meas.measNum);
-	terminal->println("  ug/m3");
-	terminal->println();
+	terminal.print("PM1.0: ");
+	terminal.print(PM_1_0_meas.value / PM_1_0_meas.measNum);
+	terminal.println("  ug/m3");
+	terminal.print("PM2.5: ");
+	terminal.print(PM_2_5_meas.value / PM_2_5_meas.measNum);
+	terminal.println("  ug/m3");
+	terminal.print("PM1 0: ");
+	terminal.print(PM_10_meas.value / PM_10_meas.measNum);
+	terminal.println("  ug/m3");
+	terminal.println();
 
-	terminal->flush();
+	terminal.flush();
 
-	terminal->print("eCO2: ");
-	terminal->print(eCO2_meas.value / eCO2_meas.measNum);
-	terminal->println("  ppm");
-	terminal->println();
+	terminal.print("eCO2: ");
+	terminal.print(eCO2_meas.value / eCO2_meas.measNum);
+	terminal.println("  ppm");
+	terminal.println();
 
-	terminal->print("TVOC: ");
-	terminal->print(TVOC_meas.value / TVOC_meas.measNum);
-	terminal->println("  ppb");
-	terminal->println();
+	terminal.print("TVOC: ");
+	terminal.print(TVOC_meas.value / TVOC_meas.measNum);
+	terminal.println("  ppb");
+	terminal.println();
 
-	terminal->flush();
+	terminal.flush();
 
-	if (myUI->currUI_Params.dispMode == standard) {
-		terminal->println("Display standard");
-		switch (myUI->currUI_Params.dispParam) {
+	/*if (myUI.currUI_Params.dispMode == standard) {
+		terminal.println("Display standard");
+		switch (myUI.currUI_Params.dispParam) {
 			case total:
-				terminal->println("Display total quality");
+				terminal.println("Display total quality");
 				break;
 			case temp:
-				terminal->println("Display temperature");
+				terminal.println("Display temperature");
 				break;
 			case humid:
-				terminal->println("Display humidity");
+				terminal.println("Display humidity");
 				break;
 			case eCO2:
-				terminal->println("Display eCO2");
+				terminal.println("Display eCO2");
 				break;
 			case TVOC:
-				terminal->println("Display TVOC");
+				terminal.println("Display TVOC");
 				break;
 			case PM2_5:
-				terminal->println("Display PM2_5");
+				terminal.println("Display PM2_5");
 				break;
 			default:
-				terminal->println("Display none parametr");
+				terminal.println("Display none parametr");
 		}
-	} else if (myUI->currUI_Params.dispMode == multi) {
-		terminal->println("Display multi");
+	} else if (myUI.currUI_Params.dispMode == multi) {
+		terminal.println("Display multi");
 		for (int i = 0; i < MULTI_MODE_PARAM_NUM; i++) {
-			switch (myUI->currUI_Params.multiModeStruct.paramsArr[i]) {
+			switch (myUI.currUI_Params.multiModeStruct.paramsArr[i]) {
 				case total:
-					terminal->println("Display total quality");
+					terminal.println("Display total quality");
 					break;
 				case temp:
-					terminal->println("Display temperature");
+					terminal.println("Display temperature");
 					break;
 				case humid:
-					terminal->println("Display humidity");
+					terminal.println("Display humidity");
 					break;
 				case eCO2:
-					terminal->println("Display eCO2");
+					terminal.println("Display eCO2");
 					break;
 				case TVOC:
-					terminal->println("Display TVOC");
+					terminal.println("Display TVOC");
 					break;
 				case PM2_5:
-					terminal->println("Display PM2_5");
+					terminal.println("Display PM2_5");
 					break;
 				default:
-					terminal->println("Display none parametr");
+					terminal.println("Display none parametr");
 			}
 		}
-	} else if (myUI->currUI_Params.dispMode == night) {
-		switch (myUI->currUI_Params.dispParam) {
+	} else if (myUI.currUI_Params.dispMode == night) {
+		switch (myUI.currUI_Params.dispParam) {
 			case total:
-				terminal->println("Display total quality");
+				terminal.println("Display total quality");
 				break;
 			case temp:
-				terminal->println("Display temperature");
+				terminal.println("Display temperature");
 				break;
 			case humid:
-				terminal->println("Display humidity");
+				terminal.println("Display humidity");
 				break;
 			case eCO2:
-				terminal->println("Display eCO2");
+				terminal.println("Display eCO2");
 				break;
 			case TVOC:
-				terminal->println("Display TVOC");
+				terminal.println("Display TVOC");
 				break;
 			case PM2_5:
-				terminal->println("Display PM2_5");
+				terminal.println("Display PM2_5");
 				break;
 			default:
-				terminal->println("Display none parametr");
+				terminal.println("Display none parametr");
 		}
 	} else {
-		switch (myUI->currUI_Params.dispEffect) {
+		switch (myUI.currUI_Params.dispEffect) {
 			case snake:
-				terminal->println("Display snake effect");
+				terminal.println("Display snake effect");
 				break;
 			case randomPixel:
-				terminal->println("Display random pixel effect");
+				terminal.println("Display random pixel effect");
 				break;
 			case fade:
-				terminal->println("Display fade effect");
+				terminal.println("Display fade effect");
 				break;
 			case christmasTree:
-				terminal->println("Display Christmas Tree effect");
+				terminal.println("Display Christmas Tree effect");
 				break;
 			default:
 				break;
 		}
-	}
+	}*/
 
-	terminal->flush();
+	terminal.flush();
 }
 
 BLYNK_WRITE(V1) {
@@ -286,21 +273,21 @@ BLYNK_WRITE(V1) {
 	int green = param[1].asInt();
 	int blue = param[2].asInt();
 
-	myUI->ext_setStaticColor(red, green, blue);
+	//myUI.ext_setStaticColor(red, green, blue);
 }
 
 BLYNK_WRITE(V2) {
-	myUI->ext_setBrightness(param.asInt());
+	//myUI.ext_setBrightness(param.asInt());
 }
 
 BLYNK_WRITE(V3) {
-	myUI->ext_changeDispMode(param.asInt());
+	//myUI.ext_changeDispMode(param.asInt());
 }
 
 BLYNK_WRITE(V4) {
-	myUI->ext_changeDispParam(param.asInt());
+	//myUI.ext_changeDispParam(param.asInt());
 }
 
 BLYNK_WRITE(V5) {
-	myUI->ext_changeDispEffect(param.asInt());
+	//myUI.ext_changeDispEffect(param.asInt());
 }
