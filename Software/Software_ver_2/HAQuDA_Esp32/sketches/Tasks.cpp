dafbb2c6d6c8b659982c86b86ed8f83258b993d7
IlorDash <ilordash@mail.ru>
2022-08-05 00:57:45 +0300

    deleted unused delays, changed myUI fields according to changes in myUI class

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/Tasks.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/Tasks.cpp
@@ -6,31 +6,26 @@ uint8_t builtIn_LED;
 
 void WS2812_EffectsTaskCode(void *parameter) {
 	while (true) {
-		switch (myUI_tasks->currUI_Params.dispEffect) {
+		switch (HAQuDA_UI::dispEffect) {
 			case snake: {
 				WS2812_clear();
-				vTaskDelay(100 / portTICK_PERIOD_MS);
 				for (int i = 0; i < LED_NUM_PIXELS; i++) {
 					int pixelNum = 0;
-					for (int j = 0; j < myUI_tasks->currUI_Params.effectParams.snakeTailLength; j++) {
+					for (int j = 0; j < myUI_tasks->effectParams.snakeTailLength; j++) {
 						pixelNum = j + i;
 						pixelNum = (pixelNum > LED_NUM_PIXELS) ? LED_NUM_PIXELS : pixelNum;
-						WS2812_setPixelColor(pixelNum, myUI_tasks->currUI_Params.effectParams.snakeColor);
-						WS2812_show();
-						vTaskDelay(myUI_tasks->currUI_Params.effectParams.snakeSpeed / portTICK_PERIOD_MS);
+						WS2812_setPixelColor(pixelNum, myUI_tasks->effectParams.snakeColor);
+						WS2812_show(myUI_tasks->effectParams.snakeSpeed);
 					}
 					WS2812_setPixelColor(i, 0);
-					if (myUI_tasks->currUI_Params.dispEffect != snake) {
+					if (myUI_tasks->dispEffect != snake) {
 						break;
 					}
 				}
-				vTaskDelay(1000 / portTICK_PERIOD_MS);
-
 				break;
 			}
 			case randomPixel: {
 				WS2812_clear();
-				vTaskDelay(100 / portTICK_PERIOD_MS);
 				vector<int> pixelEnArr;
 				for (int i = 0; i < LED_NUM_PIXELS; i++) {
 					int pixelNum = random(0, LED_NUM_PIXELS);
@@ -40,9 +35,8 @@ void WS2812_EffectsTaskCode(void *parameter) {
 					pixelEnArr.push_back(pixelNum);
 
 					WS2812_setPixelColor(pixelNum, ((uint32_t)random(0, 255) << 16) | ((uint32_t)random(0, 255) << 8) | random(0, 255));
-					WS2812_show();
-					vTaskDelay(myUI_tasks->currUI_Params.effectParams.randomSpeed / portTICK_PERIOD_MS);
-					if (myUI_tasks->currUI_Params.dispEffect != randomPixel) {
+					WS2812_show(myUI_tasks->effectParams.randomSpeed);
+					if (myUI_tasks->dispEffect != randomPixel) {
 						break;
 					}
 				}
@@ -51,13 +45,11 @@ void WS2812_EffectsTaskCode(void *parameter) {
 			}
 			case fade: {
 				WS2812_clear();
-				vTaskDelay(100 / portTICK_PERIOD_MS);
-				WS2812_fillColor(myUI_tasks->currUI_Params.effectParams.fadeColor);
+				WS2812_fillColor(myUI_tasks->effectParams.fadeColor);
 				for (int j = 100; j > 0; j = j - 2) {
 					WS2812_setBrightnessPerCent(j);
-					WS2812_show();
-					vTaskDelay(myUI_tasks->currUI_Params.effectParams.fadeSpeed / portTICK_PERIOD_MS);
-					if (myUI_tasks->currUI_Params.dispEffect != fade) {
+					WS2812_show(myUI_tasks->effectParams.fadeSpeed);
+					if (myUI_tasks->dispEffect != fade) {
 						break;
 					}
 				}
@@ -67,20 +59,17 @@ void WS2812_EffectsTaskCode(void *parameter) {
 				// WS2812_ChristmasTree(400);
 				break;
 			}
-			default: {
-				vTaskDelay(500 / portTICK_PERIOD_MS);
+			default:
 				break;
-			}
 		}
 	}
 }
 
 void WS2812_ErrorDispTaskCode(void *parameter) {
 	while (true) {
-		switch (HAQuDA_ErrorHandler::currErrorToDisp) {
+		switch (HAQuDA_UI::dispError) {
 			case failedToConnectToWiFi: {
 				WS2812_clear();
-				vTaskDelay(100 / portTICK_PERIOD_MS);
 				for (int i = 0; i < LED_COLUMN_NUM; i++) {
 					for (int j = (i % 2); j < LED_ROW_NUM; j += 2) {
 						uint8_t pixelNum = i * LED_ROW_NUM + j;
@@ -88,7 +77,6 @@ void WS2812_ErrorDispTaskCode(void *parameter) {
 					}
 				}
 				WS2812_show();
-				vTaskDelay(500 / portTICK_PERIOD_MS);
 				break;
 			}
 
@@ -102,7 +90,6 @@ void WS2812_ErrorDispTaskCode(void *parameter) {
 					}
 				}
 				WS2812_show();
-				vTaskDelay(500 / portTICK_PERIOD_MS);
 				break;
 			}
 			default:
@@ -125,6 +112,13 @@ void BuiltIn_LED_Blink_TaskCode(void *parameter) {
 void createTasks(HAQuDA_UI *currUI) {
 	myUI_tasks = currUI;
 	builtIn_LED = atoi(BUILTIN_LED);
+
+	myUI_tasks->effectParams.snakeColor = COLOR_AQUA;
+	myUI_tasks->effectParams.snakeTailLength = 5;
+	myUI_tasks->effectParams.snakeSpeed = 50; // start up connection effect
+	HAQuDA_UI::dispEffect = snake;
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+	
 	xTaskCreatePinnedToCore(WS2812_EffectsTaskCode, // Function that should be called
 							"WS2812 Effects task",  // Name of the task (for debugging)
 							2048,					// Stack size (bytes)
@@ -144,11 +138,11 @@ void createTasks(HAQuDA_UI *currUI) {
 	);
 
 	xTaskCreatePinnedToCore(BuiltIn_LED_Blink_TaskCode, // Function that should be called
-							"BuiltIn LED Blink task", // Name of the task (for debugging)
-							1024,					  // Stack size (bytes)
-							NULL,					  // Parameter to pass
-							2,						  // Task priority
-							NULL,					  // Task handler
-							1						  // Task core
+							"BuiltIn LED Blink task",   // Name of the task (for debugging)
+							1024,						// Stack size (bytes)
+							NULL,						// Parameter to pass
+							1,							// Task priority
+							NULL,						// Task handler
+							1							// Task core
 	);
 }
