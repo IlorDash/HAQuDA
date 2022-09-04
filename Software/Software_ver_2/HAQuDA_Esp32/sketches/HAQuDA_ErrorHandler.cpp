96f62b49173521c28a5d62d2f3c75a782a571035
IlorDash <ilordash@mail.ru>
2022-09-04 03:26:12 +0300

    added fields for working woth DisplayManip

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
@@ -1,6 +1,13 @@
 #include "HAQuDA_ErrorHandler.h"
 
+
 HAQuDA_ErrorHandler::HAQuDA_ErrorHandler() {
+	failedToConnectToWiFi_displayParams.color = COLOR_RED;
+	failedToConnectToWiFi_displayParams.speed = 200;
+
+	failedToCreateAP_displayParams.color = COLOR_RED;
+	failedToCreateAP_displayParams.speed = 200;
+	failedToCreateAP_displayParams.tailLength = 1;
 }
 
 HAQuDA_ErrorHandler::~HAQuDA_ErrorHandler() {
--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
@@ -1,16 +1,16 @@
 #pragma once
+#include "HAQuDA_DisplayInterface_typedefs.h"
+typedef enum { noneError, failedToConnectToWiFi, failedToCreateAP } errorTypes_enum;
 
-typedef enum {
-	noneError,
-	failedToConnectToWiFi,
-	failedToCreateAP
-} errorTypes_enum;
+#define COLOR_RED 0xFF0000
 
 class HAQuDA_ErrorHandler {
   private:
-
   public:
-
 	HAQuDA_ErrorHandler();
+	static displayEffect_enum errorEffectDisp;
+	static growEffectsParams_struct failedToConnectToWiFi_displayParams;
+	static snakeEffectsParams_struct failedToCreateAP_displayParams;
+
 	~HAQuDA_ErrorHandler();
 };
