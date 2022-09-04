eee5b0604d4b8ef6f287ffd9631ef18d549579c8
IlorDash <ilordash@mail.ru>
2022-09-04 23:38:28 +0300

    added methods for displaying errors, added var of current error effect displaying

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
@@ -8,6 +8,18 @@ HAQuDA_ErrorHandler::HAQuDA_ErrorHandler() {
 	failedToCreateAP_displayParams.color = COLOR_RED;
 	failedToCreateAP_displayParams.speed = 200;
 	failedToCreateAP_displayParams.tailLength = 1;
+
+	errorEffectDisp = noneEffect;
+	
+	CurrentError = noneError;
+}
+
+void HAQuDA_ErrorHandler::FailedToConnectToWiFi() {
+	CurrentError = failedToConnectToWiFi;
+}
+
+void HAQuDA_ErrorHandler::FailedToCreateAP() {
+	CurrentError = failedToCreateAP;
 }
 
 HAQuDA_ErrorHandler::~HAQuDA_ErrorHandler() {
--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "HAQuDA_DisplayInterface_typedefs.h"
-typedef enum { noneError, failedToConnectToWiFi, failedToCreateAP } errorTypes_enum;
+typedef enum { failedToConnectToWiFi, failedToCreateAP, noneError } errorTypes_enum;
 
 #define COLOR_RED 0xFF0000
 
 class HAQuDA_ErrorHandler {
-  private:
   public:
-	HAQuDA_ErrorHandler();
-	static displayEffect_enum errorEffectDisp;
+	static displayEffectMode_enum errorEffectDisp;
 	static growEffectsParams_struct failedToConnectToWiFi_displayParams;
 	static snakeEffectsParams_struct failedToCreateAP_displayParams;
+	
+	static errorTypes_enum CurrentError;
+	
+	HAQuDA_ErrorHandler();
+
+	void FailedToConnectToWiFi();
+	void FailedToCreateAP();
 
 	~HAQuDA_ErrorHandler();
 };
