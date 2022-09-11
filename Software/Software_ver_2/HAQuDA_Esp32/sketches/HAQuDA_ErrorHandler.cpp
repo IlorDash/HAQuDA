a4757b4bcbad8c7775f3fabc843ca79098402794
IlorDash <ilordash@mail.ru>
2022-09-12 01:52:17 +0300

    added delays for displaying errors, some changes in error effects

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.cpp
@@ -12,21 +12,21 @@ upDownEffectsParams_struct HAQuDA_ErrorHandler::failedToStartSensors_displayPara
 errorTypes_enum HAQuDA_ErrorHandler::CurrentError = noneError;
 
 HAQuDA_ErrorHandler::HAQuDA_ErrorHandler() {
-	failedToConnectToWiFi_displayParams.color = COLOR_RED;
-	failedToConnectToWiFi_displayParams.speed = 50;
+	failedToConnectToWiFi_displayParams.color = COLOR_RED;	//grow
+	failedToConnectToWiFi_displayParams.speed = 200;
 
-	failedToCreateAP_displayParams.color = COLOR_RED;
-	failedToCreateAP_displayParams.speed = 50;
+	failedToCreateAP_displayParams.color = COLOR_RED;	//snake
+	failedToCreateAP_displayParams.speed = 200;
 	failedToCreateAP_displayParams.tailLength = 1;
 
-	failedToStartFS_displayParams.color = COLOR_RED;
-	failedToStartFS_displayParams.speed = 50;
+	failedToStartFS_displayParams.color = COLOR_RED;	//fade
+	failedToStartFS_displayParams.speed = 200;
 	failedToStartFS_displayParams.startBrightness = 100;
 	failedToStartFS_displayParams.stopBrightness = 0;
 	failedToStartFS_displayParams.step = 5;
 
-	failedToStartSensors_displayParams.color = COLOR_RED;
-	failedToStartSensors_displayParams.speed = 50;
+	failedToStartSensors_displayParams.color = COLOR_RED;	//upDown
+	failedToStartSensors_displayParams.speed = 200;
 	failedToStartSensors_displayParams.length = 3;
 }
 
@@ -36,23 +36,26 @@ errorTypes_enum HAQuDA_ErrorHandler::GetCurrentError() {
 
 void HAQuDA_ErrorHandler::FailedToStartFS() {
 	log_e("SPIFFS not mounted!");
-	
 	CurrentError = failedToStartFS;
+	vTaskDelay(DEFAULT_ERROR_DISPLAY_TIME / portTICK_PERIOD_MS);
 }
 
 void HAQuDA_ErrorHandler::FailedToConnectToWiFi() {
 	log_e("Failed connection to WiFi");
 	CurrentError = failedToConnectToWiFi;
+	vTaskDelay(DEFAULT_ERROR_DISPLAY_TIME / portTICK_PERIOD_MS);
 }
 
 void HAQuDA_ErrorHandler::FailedToCreateAP() {
 	log_e("Failed creating acces point");
 	CurrentError = failedToCreateAP;
+	vTaskDelay(DEFAULT_ERROR_DISPLAY_TIME / portTICK_PERIOD_MS);
 }
 
 void HAQuDA_ErrorHandler::FailedToStartSensors() {
 	log_e("Failed to start sensors");
 	CurrentError = failedToStartSensors;
+	vTaskDelay(DEFAULT_ERROR_DISPLAY_TIME / portTICK_PERIOD_MS);
 }
 
 HAQuDA_ErrorHandler::~HAQuDA_ErrorHandler() {
