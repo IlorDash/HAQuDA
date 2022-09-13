80acebff599ab5f4712a8ddec9b18214260618e8
IlorDash <ilordash@mail.ru>
2022-09-14 02:01:32 +0300

    moved all display effects method in one task (displaying errors and simple effects), for more easily controling of what displaying now

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/Tasks.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/Tasks.cpp
@@ -4,35 +4,6 @@
 HAQuDA_DisplayManip *myDisplayManip_tasks;
 uint8_t builtIn_LED;
 
-void WS2812_ErrorDisplayTaskCode(void *parameter) {
-	while (true) {
-		errorTypes_enum e = HAQuDA_ErrorHandler::GetCurrentError();
-		switch (e) {
-			case failedToStartFS: {
-				HAQuDA_DisplayManip::ShowEffectFade(HAQuDA_ErrorHandler::failedToStartFS_displayParams);
-				break;
-			}
-			case failedToConnectToWiFi: {
-				HAQuDA_DisplayManip::ShowEffectGrow(HAQuDA_ErrorHandler::failedToConnectToWiFi_displayParams);
-				break;
-			}
-
-			case failedToCreateAP: {
-				HAQuDA_DisplayManip::ShowEffectSnake(HAQuDA_ErrorHandler::failedToCreateAP_displayParams);
-				break;
-			}
-
-			case failedToStartSensors: {
-				HAQuDA_DisplayManip::ShowEffectUpDown(HAQuDA_ErrorHandler::failedToStartSensors_displayParams);
-				break;
-			}
-
-			default:
-				break;
-		}
-	}
-}
-
 void BuiltIn_LED_Blink_TaskCode(void *parameter) {
 	while (true) {
 		digitalWrite(builtIn_LED, HIGH);
@@ -44,18 +15,49 @@ void BuiltIn_LED_Blink_TaskCode(void *parameter) {
 
 void WS2812_EffectsTaskCode(void *parameter) {
 	while (true) {
-		HAQuDA_DisplayManip::stopEffect = false;
-		switch (HAQuDA_DisplayManip::GetDisplayEffect()) {
+		bool isError = (HAQuDA_DisplayManip::GetDisplayMode() == error);
+		displayEffectMode_enum effect = HAQuDA_DisplayManip::GetDisplayEffect();
+
+		switch (effect) {
 			case snake: {
-				HAQuDA_DisplayManip::ShowEffectSnake();
+				if (isError) {
+					HAQuDA_DisplayManip::ShowEffectSnake(HAQuDA_ErrorHandler::failedToCreateAP_displayParams);
+				} else {
+					HAQuDA_DisplayManip::ShowEffectSnake();
+				}
 				break;
 			}
 			case randomPixel: {
+				//				if (isError) {
+				//					HAQuDA_DisplayManip::ShowEffectRandom(HAQuDA_ErrorHandler::failedTo....);
+				//				} else {
+				//					HAQuDA_DisplayManip::ShowEffectRandom();
+				//				}
 				HAQuDA_DisplayManip::ShowEffectRandom();
 				break;
 			}
 			case fade: {
-				HAQuDA_DisplayManip::ShowEffectFade();
+				if (isError) {
+					HAQuDA_DisplayManip::ShowEffectFade(HAQuDA_ErrorHandler::failedToStartFS_displayParams);
+				} else {
+					HAQuDA_DisplayManip::ShowEffectFade();
+				}
+				break;
+			}
+			case grow: {
+				if (isError) {
+					HAQuDA_DisplayManip::ShowEffectGrow(HAQuDA_ErrorHandler::failedToConnectToWiFi_displayParams);
+				} else {
+					HAQuDA_DisplayManip::ShowEffectGrow();
+				}
+				break;
+			}
+			case upDown: {
+				if (isError) {
+					HAQuDA_DisplayManip::ShowEffectUpDown(HAQuDA_ErrorHandler::failedToStartSensors_displayParams);
+				} else {
+					HAQuDA_DisplayManip::ShowEffectUpDown();
+				}
 				break;
 			}
 			case christmasTree: {
@@ -79,15 +81,6 @@ void createTasks() {
 							1,						// Task priority
 							NULL,					// Task handler
 							1						// Task core
-	);
-
-	xTaskCreatePinnedToCore(WS2812_ErrorDisplayTaskCode, // Function that should be called
-							"WS2812 Error Disp task",	// Name of the task (for debugging)
-							2048,						 // Stack size (bytes)
-							NULL,						 // Parameter to pass
-							1,							 // Task priority
-							NULL,						 // Task handler
-							1							 // Task core
 	);
 
 	xTaskCreatePinnedToCore(BuiltIn_LED_Blink_TaskCode, // Function that should be called
