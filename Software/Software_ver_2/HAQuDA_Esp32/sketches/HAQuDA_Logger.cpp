635b3f031f029fa25e06889b7615a0f18e3897b7
IlorDash <ilordash@mail.ru>
2022-09-18 21:25:01 +0300

    fixed error with un-initialized char array and strcat

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.cpp
@@ -2,6 +2,8 @@
 
 #include "HAQuDA_WebServer.h"
 
+#define MSG_MAX_LEN 64
+
 #define ERROR_MSG_PREFIX "ERROR: "
 #define INFO_MSG_PREFIX "Info: "
 
@@ -9,15 +11,16 @@ HAQuDA_Logger::HAQuDA_Logger() {
 }
 
 void HAQuDA_Logger::LogInfo(const char *msg) {
-	char *info_msg = strcat(INFO_MSG_PREFIX, msg);
+	char info_msg[MSG_MAX_LEN] = INFO_MSG_PREFIX;
+	strcat(info_msg, msg);
 	log_i("%s%s", INFO_MSG_PREFIX, msg);
 	if (HAQuDA_WebServer::GetWebServerStarted()) {
 		HAQuDA_WebServer::WebSerialPrint(info_msg);
 	}
-	
 }
 void HAQuDA_Logger::LogError(const char *msg) {
-	char *error_msg = strcat(ERROR_MSG_PREFIX, msg);
+	char error_msg[MSG_MAX_LEN] = ERROR_MSG_PREFIX;
+	strcat(error_msg, msg);
 	log_e("%s%s", ERROR_MSG_PREFIX, msg);
 	if (HAQuDA_WebServer::GetWebServerStarted()) {
 		HAQuDA_WebServer::WebSerialPrint(error_msg);
