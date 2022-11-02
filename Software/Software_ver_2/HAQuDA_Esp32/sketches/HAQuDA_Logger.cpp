f0c95a5efeea864f80ac17b182f0a476e429027d
IlorDash <ilordash@mail.ru>
2022-11-02 23:19:46 +0300

    refactored LogInfo and Log Error methods, created methods for reading stored in SPIFFS logs

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.cpp
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.cpp
@@ -3,49 +3,74 @@
 #include "HAQuDA_WebServer.h"
 #include "HAQuDA_FileStorage.h"
 
-#define MSG_MAX_LEN 57
-#define MSG_PREFIX_LEN 5
-#define MSG_DATA_MAX_LEN 32
+void HAQuDA_Logger::prepareLogInfoText(LogMsgStruct *log_i_struct, char *log_i_text, const char *msg) {
 
-#define ERROR_MSG_PREFIX "ERROR: "
-#define INFO_MSG_PREFIX "Info: "
-
-#define CANT_GET_TIME_PREFIX "Can't get time"
-
-typedef struct {
-	char timestamp[DATE_TIME_STR_LEN + 1] = {0};
-	char prefix[MSG_PREFIX_LEN] = {0};
-	char data[MSG_DATA_MAX_LEN] = {0};
-} LogMsgStruct;
-
-void HAQuDA_Logger::LogInfo(const char *msg) {
-	LogMsgStruct infoLog;
-
-	if (!HAQuDA_TimeHelper_Singleton::getInstance()->GetDateTime(infoLog.timestamp)) {
-		strncpy(infoLog.timestamp, CANT_GET_TIME_PREFIX, strlen(CANT_GET_TIME_PREFIX));
+	if (!HAQuDA_TimeHelper_Singleton::getInstance()->GetDateTime(log_i_struct->timestamp)) {
+		strncpy(log_i_struct->timestamp, CANT_GET_TIME_PREFIX, strlen(CANT_GET_TIME_PREFIX));
 	}
-	strncpy(infoLog.prefix, INFO_MSG_PREFIX, sizeof(INFO_MSG_PREFIX));
-	strncpy(infoLog.data, msg, MSG_DATA_MAX_LEN);
-	log_i("%s%s%s", infoLog.timestamp, infoLog.prefix, infolog.data);
-	char info_msg[MSG_MAX_LEN] = {0};
-	snprintf(info_msg, MSG_MAX_LEN, "%s%s%s", infoLog.timestamp, infoLog.prefix, infoLog.data);
-	if (HAQuDA_WebServer::GetWebServerStarted()) {
-		HAQuDA_WebServer::WebSerialPrint(info_msg);
-	}
+	strncpy(log_i_struct->prefix, INFO_MSG_PREFIX, sizeof(INFO_MSG_PREFIX));
+	strncpy(log_i_struct->data, msg, MSG_DATA_MAX_LEN);
+
+	snprintf(log_i_text, MSG_MAX_LEN, "%s%s%s", log_i_struct->timestamp, log_i_struct->prefix, log_i_struct->data);
 }
-void HAQuDA_Logger::LogError(const char *msg) {
-	LogMsgStruct infoLog;
-	if (!HAQuDA_TimeHelper_Singleton::getInstance()->GetDateTime(infoLog.timestamp)) {
-		strncpy(infoLog.timestamp, CANT_GET_TIME_PREFIX, strlen(CANT_GET_TIME_PREFIX));
-	}
-	strncpy(infoLog.prefix, ERROR_MSG_PREFIX, sizeof(ERROR_MSG_PREFIX));
-	strncpy(infoLog.data, msg, MSG_DATA_MAX_LEN);
-	log_i("%s%s%s", infoLog.timestamp, infoLog.prefix, infolog.data);
-	char error_msg[MSG_MAX_LEN] = {0};
-	snprintf(error_msg, MSG_MAX_LEN, "%s%s%s", infoLog.timestamp, infoLog.prefix, infoLog.data);
+
+void HAQuDA_Logger::LogInfo(const char *msg_to_show) {
+	LogMsgStruct logInfoStruct;
+	char logInfoText[MSG_MAX_LEN] = {0};
+
+	HAQuDA_Logger::prepareLogInfoText(&logInfoStruct, logInfoText, msg_to_show);
+
+	log_i("%s%s%s", logInfoText.timestamp, logInfoText.prefix, logInfoText.data);
+
 	if (HAQuDA_WebServer::GetWebServerStarted()) {
-		HAQuDA_WebServer::WebSerialPrint(error_msg);
+		HAQuDA_WebServer::WebSerialPrint(logInfoText);
 	}
+
+	HAQuDA_FileStorage::WriteFile(FILE_NAME_LOGS, (uint8_t *)&logInfoStruct, sizeof(logInfoStruct));
+}
+
+void HAQuDA_Logger::prepareLogErrorText(LogMsgStruct *log_e_struct, char *log_e_text, const char *msg) {
+
+	if (!HAQuDA_TimeHelper_Singleton::getInstance()->GetDateTime(log_e_struct->timestamp)) {
+		strncpy(log_e_struct->timestamp, CANT_GET_TIME_PREFIX, strlen(CANT_GET_TIME_PREFIX));
+	}
+	strncpy(log_e_struct->prefix, ERROR_MSG_PREFIX, sizeof(ERROR_MSG_PREFIX));
+	strncpy(log_e_struct->data, msg, MSG_DATA_MAX_LEN);
+
+	snprintf(log_e_text, MSG_MAX_LEN, "%s%s%s", log_e_struct->timestamp, log_e_struct->prefix, log_e_struct->data);
+}
+
+void HAQuDA_Logger::LogError(const char *msg_to_show) {
+	LogMsgStruct logErrorStruct;
+	char logErrorText[MSG_MAX_LEN] = {0};
+
+	HAQuDA_Logger::prepareLogErrorText(&logErrorStruct, logErrorText, msg_to_show);
+
+	log_i("%s%s%s", logErrorStruct.timestamp, logErrorStruct.prefix, logErrorStruct.data);
+
+	if (HAQuDA_WebServer::GetWebServerStarted()) {
+		HAQuDA_WebServer::WebSerialPrint(logErrorText);
+	}
+
+	HAQuDA_FileStorage::WriteFile(FILE_NAME_LOGS, (uint8_t *)&logErrorStruct, sizeof(logErrorStruct));
+}
+
+LogMsgStruct HAQuDA_Logger::GetStored_Log(int num) {
+	LogMsgStruct logMsg;
+
+	uint8_t pos = num * sizeof(LogMsgStruct);
+
+	HAQuDA_FileStorage::ReadFile(FILE_NAME_LOGS, pos, (uint8_t *)&logMsg, sizeof(LogMsgStruct));
+	return logMsg;
+}
+
+void HAQuDA_Logger::GetStored_Log(int num, char *text) {
+	LogMsgStruct logMsg = HAQuDA_Logger::GetStored_Log(num);
+	snprintf(text, MSG_MAX_LEN, "%s%s%s", logMsg.timestamp, logMsg.prefix, logMsg.data);
+}
+
+int HAQuDA_Logger::GetStored_LogsNum() {
+	return HAQuDA_FileStorage::FileSize(FILE_NAME_LOGS) / sizeof(LogMsgStruct);
 }
 
 HAQuDA_Logger::~HAQuDA_Logger() {
--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.h
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_Logger.h
@@ -2,10 +2,32 @@
 
 #include "HAQuDA_TimeHelper.h"
 
+#define MSG_MAX_LEN 57
+#define MSG_PREFIX_LEN 5
+#define MSG_DATA_MAX_LEN 32
+
+#define ERROR_MSG_PREFIX "ERROR: "
+#define INFO_MSG_PREFIX "Info: "
+
+#define CANT_GET_TIME_PREFIX "Can't get time"
+
+typedef struct {
+	char timestamp[DATE_TIME_STR_LEN + 1] = {0};
+	char prefix[MSG_PREFIX_LEN] = {0};
+	char data[MSG_DATA_MAX_LEN] = {0};
+} LogMsgStruct;
+
 class HAQuDA_Logger {
+	static void prepareLogInfoText(LogMsgStruct *log_i_struct, char *log_i_text, const char *msg);
+	static void prepareLogErrorText(LogMsgStruct *log_e_struct, char *log_e_text, const char *msg);
+	static LogMsgStruct GetStored_Log(int num);
+
   public:
-	explicit  HAQuDA_Logger();
+	explicit HAQuDA_Logger();
 	static void LogInfo(const char *msg);
 	static void LogError(const char *msg);
+
+	static void GetStored_Log(int num, char *text);
+	static int GetStored_LogsNum();
 	~HAQuDA_Logger();
 };
