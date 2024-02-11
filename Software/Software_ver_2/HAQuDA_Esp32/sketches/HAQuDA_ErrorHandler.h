60f6736a7c9716c878eb91b85e55e0c119857932
Ilya Orazov <ilordash@mail.ru>
2024-02-11 16:35:34 +0300

    use EffectsHandler methods instead of DiplayManip in ErrorHandler

--- Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
+++ Software/Software_ver_2/HAQuDA_Esp32/sketches/HAQuDA_ErrorHandler.h
@@ -1,37 +1,34 @@
 #pragma once
-#include "HAQuDA_DisplayManip_typedefs.h"
 
-typedef enum { failedToConnectToWiFi, failedToCreateAP, failedToStartFS, failedToStartSensors, noneError } errorTypes;
+#include "HAQuDA_EffectsHandler.h"
+
+typedef enum { failedToConnectToWiFi, failedToCreateAP, failedToStartFS, failedToStartSensors, failedToUpdateNTP, noneError } error_type;
 
 #define DEFAULT_ERROR_DISPLAY_TIME 5000 // 5 sec
 
-class HAQuDA_ErrorHandler {
-	static effect_mode errorEffectDisp;
+class HAQuDA_ErrorHandler : HAQuDA_EffectsHandler {
+	static error_type currentError;
 
-	static errorTypes currentError;
-
-	static grow_effect_params failedToConnectToWiFiEffect;
-	static snake_effect_params failedToCreateAPEffect;
-
-	static fade_effect_params failedToStartFSEffect;
-
-	static random_effect_params failedToStartSensorsEffect;
-
-	static void setErrorToDisplayManip(const effect_mode new_error);
-	static void resetErrorOnDisplayManip();
+	static snake_params failedToCreateAPEffect;
+	static growing_params failedToConnectToWiFiEffect;
+	static random_led_params failedToStartSensorsEffect;
+	static fading_params failedToStartFSEffect;
+	static fading_params failedToUpdateNTPEffect;
 
   public:
 	HAQuDA_ErrorHandler();
 
-	static void ShowError();
+	static void Show();
 
-	static errorTypes GetCurrentError();
+	static error_type GetCurrentError() { return currentError; }
 
 	void FailedToConnectToWiFi();
 	void FailedToCreateAP();
 	void FailedToStartFS();
 	void FailedToStartSensors();
-	void ClearCurrentError();
+	void FailedToUpdateNTP();
+	void FailedToGetMeas(const char *msg);
+	void ClearError();
 
 	~HAQuDA_ErrorHandler();
 };
